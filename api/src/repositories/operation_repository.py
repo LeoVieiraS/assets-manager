541240cc96431da7ef7ae470e2fe842a30864e86
Leonardo <leosantos_vieira@hotmail.com>
2022-10-11 00:08:59 -0300

    added repositories

---
+++ api/src/repositories/operation_repository.py
@@ -0,0 +1,40 @@
+import traceback
+from abc import abstractmethod
+
+from flask import jsonify
+from models import db
+from models.operation import Operation, OperationSchema
+from typing import List
+
+
+class OperationRepository:
+    @abstractmethod
+    def insert(op: Operation) -> Operation:
+        try:
+            db.session.add(op)
+            db.session.commit()
+        except Exception as e:
+            print(traceback.format_exception(type(e), e, e.__traceback__))
+            return None, (jsonify({"error": "Operation not inserted"}), 500)
+        return OperationRepository.find_latest(), None
+
+    @abstractmethod
+    def find_latest() -> OperationSchema:
+        return db.session.query(Operation).order_by(Operation.id.desc()).first()
+
+    @abstractmethod
+    def delete(op: Operation):
+        try:
+            print(type(op))
+            db.session.delete(op)
+            db.session.commit()
+        except Exception as e:
+            print(traceback.format_exception(type(e), e, e.__traceback__))
+
+    @abstractmethod
+    def find_all() -> List[Operation]:
+        return Operation.query.all()
+
+    @abstractmethod
+    def find_by_id(id: int):
+        return Operation.query.filter_by(id=id).first()
---
+++ api/src/repositories/user_repository.py
@@ -0,0 +1,29 @@
+from inspect import trace
+import traceback
+from abc import abstractmethod
+from typing import Union
+from models import db
+from models.user import User
+
+
+class UserRepository:
+    @abstractmethod
+    def insert(user: User) -> User:
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except Exception as e:
+            traceback.format_exception(e)
+        return UserRepository.find_latest()
+
+    def find_latest() -> Union[User, None]:
+        return db.session.query(User).order_by(User.id.desc()).first()
+
+    @abstractmethod
+    def find_by_email(email: str) -> Union[User, None]:
+        user = User.query.filter_by(email=email).first()
+        return user
+
+    @abstractmethod
+    def delete(email: str) -> bool:
+        ...
