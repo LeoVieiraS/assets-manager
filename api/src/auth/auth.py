8022b09060dbd0fa8ea752bac54fab5be4ed3601
Leonardo <leosantos_vieira@hotmail.com>
2022-10-13 13:27:55 -0300

    added auth to create user

--- api/src/auth/auth.py
+++ api/src/auth/auth.py
@@ -19,10 +19,11 @@ def admin_authorization(f):
         payload = get_payload_from_token()
         admin = AdminRepository.find_by_id(payload["id"])
         if not admin:
-            return jsonify({"error": "Not Allowed"})
+            return jsonify({"error": "Not Allowed"}), 401
 
         return f()
 
+    wrapper.__name__ = f.__name__
     return wrapper
 
 
@@ -31,7 +32,7 @@ def user_authorization(f):
         payload = get_payload_from_token()
         user: User = UserRepository.find_by_id(payload["id"])
         if not user:
-            return jsonify({"error": "Not Allowed"})
+            return jsonify({"error": "Not Allowed"}), 401
         g.user_id = user.id
         return f()
 
--- api/src/blueprints/users.py
+++ api/src/blueprints/users.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from services.user_service import UserService
-from models import db
+from auth.auth import admin_authorization
 
 users_blueprints = Blueprint("users", __name__, url_prefix="/api/v1")
 
 
 @users_blueprints.route("/users", methods=["POST"])
+@admin_authorization
 def create():
     return UserService.create_user()
